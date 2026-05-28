469369cee1c06966121fd9117ba921b7a832ac78
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-28 15:59:08 -0300

    estructura TP7

---
+++ TP7Grupo18/Inicio.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP7Grupo18
+{
+    public partial class Inicio : System.Web.UI.Page
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
